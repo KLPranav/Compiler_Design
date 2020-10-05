fb638757456302c2586aea4dcffbc263e9cf8c48
Lakshmi Pranav Kakumanu <lakshmi_pranav@srmap.edu.in>
2020-10-05 23:32:02 +0530

    Add files via upload

--- Week 1 Language recognizer/1a.c
+++ Week 1 Language recognizer/1a.c
@@ -1,57 +1,57 @@
+//Implementation of Language recognizer for set of all strings over binary alphabet containing even number of 0’s and even number of 1’s.
 #include<stdio.h>
 void main(){
-int state=0,i=0;
-char current,input[20];
-printf("Enter input string \t :");
-scanf("%s",input);
-while((current=input[i++])!='\0'){
-switch(state)
-{
-case 0: if(current=='a')
-state=1;
-else if(current=='b')
-state=3;
-else
-{
-printf("Invalid token");
-exit(0);
-}
-break;
-case 1: if(current=='a')
-state=0;
-else if(current=='b')
-state=2;
-else
-{
-printf("Invalid token");
-exit(0);
-}
-break;
-case 2: if(current=='a')
-state=3;
-else if(current=='b')
-state=1;
-else
-{
-printf("Invalid token");
-exit(0);
-}
-break;
-case 3: if(current=='a')
-state=2;
-else if(current=='b')
-state=0;
-else
-{
-printf("Invalid token");
-
-exit(0);
-}
-break;
-}
-}
-if(state==0)
-printf("\n\nString accepted\n\n");
-else
-printf("\n\nString not accepted\n\n");
+	int state=0,i=0;
+	char current,input[20];
+	printf("Enter a string \t : ");
+	scanf("%s",input);
+	while((current=input[i++])!='\0'){
+		switch(state)
+		{
+			case 0: if(current=='0')
+						state=1;
+					else if(current=='1')
+						state=3;
+					else
+					{
+						printf("Invalid token");
+						exit(0);
+					}
+					break;
+			case 1: if(current=='0')
+						state=0;
+					else if(current=='1')
+						state=2;
+					else
+					{
+						printf("Invalid token");
+						exit(0);
+					}
+					break;
+			case 2: if(current=='0')
+						state=3;
+					else if(current=='1')
+						state=1;
+					else
+					{
+						printf("Invalid token");
+						exit(0);
+					}
+					break;
+			case 3: if(current=='0')
+						state=2;
+					else if(current=='1')
+						state=0;
+					else
+					{
+						printf("Invalid token");
+						exit(0);
+					}
+					break;
+		}
+	}
+	if(state==0)
+		printf("\n\nString is accepted\n\n");
+	else
+		printf("\n\nString is not accepted\n\n");
 }
